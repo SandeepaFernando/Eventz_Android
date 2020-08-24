967ddadbff605389638924b2601904357c105dc2
Sandeepa Fernando <sandeepapriyashan@gmail.com>
2020-08-24 23:58:26 +0530

    Event Info 80% done, bid did not added part.

--- app/src/main/java/com/example/eventz/eventInfo/Event_infoActivity.java
+++ app/src/main/java/com/example/eventz/eventInfo/Event_infoActivity.java
@@ -1,16 +1,21 @@
 package com.example.eventz.eventInfo;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.RetryPolicy;
 import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.eventz.preferences.User;
 import com.example.eventz.register.RegVenderScrollingActivity;
 import com.example.eventz.register.SkillAdapter;
 import com.example.eventz.register.SkillItem;
@@ -21,9 +26,12 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.eventz.R;
@@ -32,16 +40,31 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Event_infoActivity extends AppCompatActivity {
+    User user = new User();
     String eventId;
+    private ArrayList<EventSkillInfo> mSkillList;
+    private RecyclerView mRecyclerView;
+    private EventSkillAdapter mskillAdapter;
+    private RequestQueue mRequestQueueReg;
+    TextView titleTV, descriptionTV, venueTV, dateTV, num_peopleTV, f_nameTV, emailTV;
+    String title, description, eventDate, venue, noOfGuests, fname, email;
+    String token;
     String URL = "http://192.168.1.103:8000/events";
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
 
@@ -54,69 +77,116 @@ public class Event_infoActivity extends AppCompatActivity {
             }
         });
 
+        //-----------------------------------------------------------------------------
+        titleTV = findViewById(R.id.txtview_title);
+        descriptionTV = findViewById(R.id.txtview_description);
+        venueTV = findViewById(R.id.txtview_venue);
+        dateTV = findViewById(R.id.txtview_date);
+        num_peopleTV = findViewById(R.id.txtview_numofpeople);
+        f_nameTV = findViewById(R.id.txtview_fname);
+        emailTV = findViewById(R.id.txtview_email);
 
+
+        SharedPreferences sp = user.retrieveUserData(Objects.requireNonNull(getApplicationContext()));
+        //userType = sp.getString("USERTYPE", "");
+        token = sp.getString("KEY_TOKEN", "");
+
+
+        mSkillList = new ArrayList<>();
+        mRecyclerView = findViewById(R.id.event_skill_set);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        getEventbyid();
 
     }
 
 
-//    void getEventbyid() {
-//        final Intent intent = getIntent();
-//        eventId = intent.getStringExtra("eventId");
-//        Log.i("EXTRASITE_ID", eventId);
-//
-//        final JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, URL, null,
-//                new Response.Listener<JSONArray>() {
-//                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-//                    @Override
-//                    public void onResponse(JSONArray response) {
-//                        try {
-//
-//                            Log.i("RESPONSE", response.toString());
-//
-//                            for (int j = 0; j < response.length(); j++) {
-//                                JSONObject jsonObject = response.getJSONObject(j);
-//                                Log.i("INSIDEOBJECR", jsonObject.toString());
-//                                String skillID = String.valueOf(jsonObject.getInt("id"));
-//                                String tagName = jsonObject.getString("tagName");
-//
-//                                skillIdArr.add(skillID);
-//                                skillNameArr.add(tagName);
-//
-//                                mSkillList.add(new SkillItem(tagName));
-//
-//                            }
-//
-//                            Log.i("ARRRRR", skillIdArr.toString());
-//                            Log.i("ARRRRR", skillNameArr.toString());
-//
-//                            mskillAdapter = new SkillAdapter(RegVenderScrollingActivity.this, mSkillList);
-//                            mRecyclerView.setAdapter(mskillAdapter);
-//                            SkillAdapter.setOnItemClickListener(RegVenderScrollingActivity.this);
-//
-//                        } catch (JSONException e) {
-//                            Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_LONG).show();
-//
-//                        }
-//                    }
-//                }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                if (error.networkResponse == null) {
-//                    if (error.getClass().equals(TimeoutError.class)) {
-//                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
-//                        Log.v("VOLLEY", error.toString());
-//                    }
-//                }
-//
-//            }
-//        }) {
-//
-//        };
-//
-//        int socketTimeout = 500000;//30 seconds - change to what you want
-//        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
-//        request.setRetryPolicy(policy);
-//
-//        mRequestQueue.add(request);
-//    }
+    void getEventbyid() {
+        mRequestQueueReg = Volley.newRequestQueue(this);
+        final Intent intent = getIntent();
+        eventId = intent.getStringExtra("eventId");
+        Log.i("EXTRASITE_ID", eventId);
+
+        String finalURL = URL + "?eventId=" + eventId;
+
+        final JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, finalURL, null,
+                new Response.Listener<JSONArray>() {
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+
+                            Log.i("RESPONSEINFO", response.toString());
+
+                            for (int j = 0; j < response.length(); j++) {
+                                JSONObject jsonObject = response.getJSONObject(j);
+                                Log.i("INSIDEOBJECR", jsonObject.toString());
+                                String id = String.valueOf(jsonObject.getInt("id"));
+                                title = jsonObject.getString("title");
+                                description = jsonObject.getString("description");
+                                eventDate = jsonObject.getString("eventDate");
+                                venue = jsonObject.getString("venue");
+                                noOfGuests = jsonObject.getString("noOfGuests");
+
+                                JSONArray tags = jsonObject.getJSONArray("eventTags");
+                                Log.i("TAGARR", tags.toString());
+
+                                for (int i = 0; i < tags.length(); i++) {
+                                    JSONObject tagObject = tags.getJSONObject(i);
+                                    String tagName = tagObject.getString("tagName");
+
+                                    mSkillList.add(new EventSkillInfo(tagName));
+                                }
+
+                                JSONObject organizerobj = jsonObject.getJSONObject("organizer");
+                                fname = organizerobj.getString("first_name");
+                                email = organizerobj.getString("email");
+
+                            }
+
+                            mskillAdapter = new EventSkillAdapter(Event_infoActivity.this, mSkillList);
+                            mRecyclerView.setAdapter(mskillAdapter);
+
+                            titleTV.setText(title);
+                            descriptionTV.setText(description);
+                            venueTV.setText(venue);
+                            dateTV.setText(eventDate);
+                            num_peopleTV.setText(noOfGuests);
+                            f_nameTV.setText(fname);
+                            emailTV.setText(email);
+
+                        } catch (JSONException e) {
+                            Log.i("JSONException", e.getMessage());
+                            Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (error.networkResponse == null) {
+                    if (error.getClass().equals(TimeoutError.class)) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.v("VOLLEY", error.toString());
+                    }
+                }
+
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", "Token " + token);
+
+                return params;
+            }
+        };
+
+
+        int socketTimeout = 500000;//30 seconds - change to what you want
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        request.setRetryPolicy(policy);
+
+        mRequestQueueReg.add(request);
+    }
 }
