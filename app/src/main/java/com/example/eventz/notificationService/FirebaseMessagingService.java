6cd54d82117d9e96b7036fccf898c351734e1f92
Sandeepa Fernando <sandeepapriyashan@gmail.com>
2020-11-04 23:39:02 +0530

    FCM completed, Bug in App Kill state.

--- app/src/main/java/com/example/eventz/notificationService/FirebaseMessagingService.java
+++ app/src/main/java/com/example/eventz/notificationService/FirebaseMessagingService.java
@@ -1,34 +1,83 @@
 package com.example.eventz.notificationService;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
+import com.example.eventz.R;
+import com.example.eventz.eventInfo.Event_infoActivity;
 import com.example.eventz.preferences.User;
 import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
 
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
     String TAG = "FCM_PAYLOAD-";
     User user = new User();
-    String userType, userName, userEmail, userId, token;
+    String userId;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-//        SharedPreferences sp = user.retrieveUserData(getApplicationContext());
-//        userId = sp.getString("USER_ID", "User Email");
-//        token = sp.getString("KEY_TOKEN", "");
+        SharedPreferences sp = user.retrieveUserData(getApplicationContext());
+        userId = sp.getString("USER_ID", "");
 
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "DATA: " + remoteMessage.getData());
 
-        Log.i("NOTIFICATION", String.valueOf(remoteMessage.getData()));
-//        Log.d(TAG, "From: " + remoteMessage.getFrom());
-//        Map<String, String> payload = remoteMessage.getData();
-//        Log.d(TAG, "Payload" + payload);
+        try {
+            JSONObject object = new JSONObject(Objects.requireNonNull(remoteMessage.getData().get("data")));
+            String eventId = object.getString("eventId");
+            Log.d(TAG, "eventId: " + eventId);
+
+            Intent intent = new Intent(this, Event_infoActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.putExtra("eventId", eventId);
+            intent.putExtra("userId", userId);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+            String channelId = "Default";
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                    .setContentTitle(remoteMessage.getData().get("title"))
+                    .setContentText(remoteMessage.getData().get("body"))
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent);
+
+            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel(channelId, "Default channel", NotificationManager.IMPORTANCE_DEFAULT);
+                manager.createNotificationChannel(channel);
+            }
+            manager.notify((int) System.currentTimeMillis(), builder.build());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void onNewToken(@NonNull String s) {
         super.onNewToken(s);
 
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
     }
 }
