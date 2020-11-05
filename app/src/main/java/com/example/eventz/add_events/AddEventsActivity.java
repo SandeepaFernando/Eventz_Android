b05e0e5820ae4d3b7787fe4e1161ffc8df83b01e
Sandeepa Fernando <sandeepapriyashan@gmail.com>
2020-11-05 23:34:08 +0530

    Add Event modified

--- app/src/main/java/com/example/eventz/add_events/AddEventsActivity.java
+++ app/src/main/java/com/example/eventz/add_events/AddEventsActivity.java
@@ -71,10 +71,12 @@ public class AddEventsActivity extends AppCompatActivity implements SkillAdapter
     Button btn_skillPredict;
     Button btn_numOfVendors;
     Button btn_post;
+    Button btn_getbtnAvailable;
     private RequestQueue mRequestQueuePredict;
     private RequestQueue mRequestQueueaddSkills;
     private RequestQueue mRequestQueueNumOfVendors;
     private RequestQueue mRequestQueuePost;
+    private RequestQueue mRequestQueueDate;
     private ArrayList<SkillItemAddEvent> mSkillList;
     private ArrayList<SkillItemAddEvent> mSkillListAdd;
     private RecyclerView mRecyclerViewPredict;
@@ -85,6 +87,7 @@ public class AddEventsActivity extends AppCompatActivity implements SkillAdapter
     private ArrayList<String> categoryArr;
     LinearLayout linearLayout_predict;
     LinearLayout linearLayout_numOfVendors;
+    LinearLayout checkDateAvailability;
     ImageView addSkillsImg;
     ImageView clearnNmViewImg;
     Calendar calendar;
@@ -93,6 +96,7 @@ public class AddEventsActivity extends AppCompatActivity implements SkillAdapter
     TextView numPlattinum;
     TextView numGold;
     TextView numSilver;
+    TextView txt_check_date_availability;
     private int mStatusCode = 0;
     String dateIn_IN;
 
@@ -117,6 +121,7 @@ public class AddEventsActivity extends AppCompatActivity implements SkillAdapter
         textInputTimeLayout = findViewById(R.id.text_input_time_add_event);
         linearLayout_predict = findViewById(R.id.predicted_skills_layout);
         linearLayout_numOfVendors = findViewById(R.id.num_of_vendor_skills_layout);
+        checkDateAvailability = findViewById(R.id.get_available_date_layout);
         addSkillsImg = findViewById(R.id.img_add_skills_addevent);
         textInputDate = findViewById(R.id.text_input_pick_date_add_event);
         textInputDate.setFocusableInTouchMode(false);
@@ -126,10 +131,12 @@ public class AddEventsActivity extends AppCompatActivity implements SkillAdapter
         numGold = findViewById(R.id.txt_num_of_gold);
         numSilver = findViewById(R.id.txt_num_of_silver);
         clearnNmViewImg = findViewById(R.id.img_clear_all_num_view);
+        txt_check_date_availability = findViewById(R.id.txt_check_date_availability);
 
         btn_skillPredict = findViewById(R.id.btn_predict_skills);
         btn_numOfVendors = findViewById(R.id.btn_num_of_vendors);
         btn_post = findViewById(R.id.button_post_event_now);
+        btn_getbtnAvailable = findViewById(R.id.btn_get_available_date);
 
         mRequestQueuePredict = Volley.newRequestQueue(this);
         mRecyclerViewPredict = findViewById(R.id.recyclerView__skill_predict);
@@ -256,6 +263,73 @@ public class AddEventsActivity extends AppCompatActivity implements SkillAdapter
             }
         });
 
+        btn_getbtnAvailable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getAvailableDate();
+            }
+        });
+
+    }
+
+    private void getAvailableDate() {
+        mRequestQueueDate = Volley.newRequestQueue(this);
+        String dateTime = textInputDate.getText().toString() + " " + textInputTime.getText().toString();
+        String tagString = skillIdArr.toString();
+        String tags = tagString.replaceAll("[\\[\\]() ]", "");
+        String url = getString(R.string.ip);
+        String URL = url + "checkDateAvailability?date=" + dateTime +":00"+ "&tags=" + tags.trim();
+        Log.i("getAvailableDate", URL);
+
+        final JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL, null,
+                new Response.Listener<JSONObject>() {
+                    @SuppressLint("SetTextI18n")
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+                            Log.i("RESPONSE", response.toString());
+
+                            String result = response.getString("response");
+
+
+                            // set visible
+                            checkDateAvailability.setVisibility(View.VISIBLE);
+                            txt_check_date_availability.setText(result);
+
+
+                        } catch (JSONException e) {
+                            Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (error.networkResponse == null) {
+                    if (error.getClass().equals(TimeoutError.class)) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.v("VOLLEY", error.toString());
+                    }
+                }
+
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", "Token " + token);
+
+                return params;
+            }
+        };
+
+        int socketTimeout = 500000;//30 seconds - change to what you want
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        request.setRetryPolicy(policy);
+
+        mRequestQueueDate.add(request);
     }
 
     private void postAdd() {
